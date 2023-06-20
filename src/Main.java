ce9991cfcfdbe08d6b8ad50a51b7ecd0290b6c29
diaboxic <shafayat.da5hfyt@gmail.com>
2023-06-20 14:50:05 +0600

    Assignment3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Rectangle rec = new Rectangle(4,5);
-		System.out.println("Rectangle");
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("One side of Rectangle: ");
+		int a = scn.nextInt();
+		System.out.println("Another side of Rectangle: ");
+		int b = scn.nextInt();
+		System.out.println("Side of Square: ");
+		int c = scn.nextInt();
+
+		
+		Rectangle rec = new Rectangle(a,b);
+		System.out.println("\nRectangle");
 		System.out.println("Area: "+rec.area());
 		System.out.println("Perimeter: "+rec.perimeter());
 		
-		Square sq = new Square(7);
+		Square sq = new Square(c);
 		System.out.println("\nSquare");
 		System.out.println("Area: "+sq.area());
 		System.out.println("Perimeter: "+sq.perimeter());
+		
+		scn.close();
 
 	}
 
